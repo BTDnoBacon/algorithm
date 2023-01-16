177596aa3d65f00d67367953748b291f9bef9129
BTDnoBacon <osabero@naver.com>
2023-01-16 17:31:41 +0900

    해결안댐

--- SWEA/1247/1247.py
+++ SWEA/1247/1247.py
@@ -3,10 +3,28 @@ sys.stdin = open('input.txt', 'r')
 
 import collections
 
+def dfs(start, end, case):
+    dict = collections.defaultdict(int)
+    queue = collections.deque()
+    queue.append(start)
+    
+
+
 
 
 N = int(input())
 for _ in range(N):
     M = int(input())
     dummy = list(map(int, input().split()))
-    print(dummy)
+    case = list()
+    start = dummy[:2]
+    end = dummy[2:4]
+    for i in range(2, M+2):
+        case.append(dummy[i*2:i*2+2])
+    dfs(start, end, case)
+
+    
+
+
+
+
