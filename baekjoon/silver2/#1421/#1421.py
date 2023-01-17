45d227fd8e705fb7c315653794bd362370f766c9
BTDnoBacon <osabero@naver.com>
2023-01-17 10:08:53 +0900

    1421 풀이

--- baekjoon/silver2/#1421/#1421.py
+++ baekjoon/silver2/#1421/#1421.py
@@ -1,10 +1,37 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-N, M, K = list(map(int, input().split()))
+
+N, C, W = list(map(int, input().split()))
 
 case = list()
 for _ in range(N):
     case.append(int(input()))
 
-print(case)
+
+dictionary = dict()
+for item in case:
+    dictionary[item] = item*W*case.count(item)
+
+top_value = max(dictionary.values())
+
+for i in range(1, max(case)+1):
+    L = i
+    check = 0
+    for tree in case:
+        div = 0
+        if tree % L == 0:
+            div = (tree // L) - 1
+        else:
+            div = tree // L
+        
+        answer = W * L * (tree // L) - div * C
+
+        if answer > 0:
+            check += answer
+        else:
+            continue
+    top_value = max(top_value, check)
+
+
+print(top_value)
