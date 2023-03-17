5f6b4aa911073cfbad71e78e4a49bfbeb68bda65
BTDnoBacon <osabero@naver.com>
2023-03-17 20:17:44 +0900

    17406 풀이

--- baekjoon/gold4/#17406/17406.py
+++ baekjoon/gold4/#17406/17406.py
@@ -1,9 +1,10 @@
 import sys
-sys.stdin = open('input.txt', 'r')
+from itertools import permutations
+# sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 
 
-def rotation_matrix(r, c, s):
+def rotation_matrix(r, c, s, matrix):
     temp = matrix[r-s][c-s]
     for j in range(c-s, c+s):
         matrix[r-s][j+1], temp = temp, matrix[r-s][j+1]
@@ -15,21 +16,36 @@ def rotation_matrix(r, c, s):
         matrix[i-1][c-s], temp = temp, matrix[i-1][c-s]
     
     if s - 1 > 0:
-        rotation_matrix(r, c, s-1)
+        rotation_matrix(r, c, s-1, matrix)
     
 
     
 
 N, M, K = map(int, input().split())
 
-matrix = [list(map(int, input().split())) for _ in range(N)]
+case = [list(map(int, input().split())) for _ in range(N)]
+dummy = []
 for _ in range(K):
     r, c, s = map(int, input().split())
-    rotation_matrix(r-1, c-1, s)
-
+    dummy.append([r, c, s])
+    # rotation_matrix(r-1, c-1, s)
+dummy = list(permutations(dummy))
+# print(dummy)
 min_value = 100 * M + 1
-for item in matrix:
-    sum_items = sum(item)
-    if sum_items < min_value:
-        min_value = sum_items
-print(min_value) 
+for item in dummy:
+    matrix = []
+    for row in case:
+        matrix.append(row[:])
+    for r, c, s in item:
+        rotation_matrix(r-1, c-1, s, matrix)
+    for item in matrix:
+        min_value = min(min_value, sum(item))
+
+print(min_value)
+
+
+# for item in matrix:
+#     sum_items = sum(item)
+#     if sum_items < min_value:
+#         min_value = sum_items
+# print(min_value) 
