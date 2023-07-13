21fc53e672f5572e05717be8304f9329ca875cdc
seungwookim <osabero@naver.com>
2023-07-13 09:15:34 +0900

    1932풀이

--- baekjoon/silver1/#1932/1932.py
+++ baekjoon/silver1/#1932/1932.py
@@ -1,8 +1,23 @@
 import sys
-sys.stdin = open('input.txt', 'r')
+# sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 
 n = int(input())
-case = [list(map(int, input().split())) for _ in range(n)]
+triangle = [list(map(int, input().split())) for _ in range(n)]
 # print(case)
 
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            triangle[i][j] += triangle[i-1][j]
+        elif j < i:
+            # print(i, triangle[i-1][j])
+            # print(i, triangle[i-1][j-1])
+            # print(i, triangle[i-1][j+1])
+
+
+            triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+        else:
+            triangle[i][j] += triangle[i-1][j-1]
+print(max(triangle[n-1]))
+# print(triangle)
